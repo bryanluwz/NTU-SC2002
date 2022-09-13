7885551c84576d7a8692c256387129564ea7873e
bryanluwz <bryanlu.my@gmail.com>
2022-09-13 15:01:16 +0800

    Initial [commit

--- src/sc2002_tutorial/VendingMachineApp.java
+++ src/sc2002_tutorial/VendingMachineApp.java
@@ -1,7 +1,7 @@
 package sc2002_tutorial;
 
 public class VendingMachineApp {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		VendingMachine vm = new VendingMachine();
 		vm.start();
 	}
