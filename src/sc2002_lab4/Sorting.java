ea0b154bec6923960de95834ceed5791ac49a2fc
bryanluwz <bryanlu.my@gmail.com>
2022-10-16 16:48:18 +0800

    lab 4 commit

--- src/sc2002_lab4/Sorting.java
+++ src/sc2002_lab4/Sorting.java
@@ -12,6 +12,7 @@ public class Sorting {
         for (int index = 0; index < list.length - 1; index++) {
             min = index;
             for (int scan = index + 1; scan < list.length; scan++)
+//                if (list[scan].compareTo(list[min]) > 0)
                 if (list[scan].compareTo(list[min]) < 0)
                     min = scan;
             // Swap the values
