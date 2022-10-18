825c1c288caa2c2614573f793d001adba0ea9735
bryanluwz <bryanlu.my@gmail.com>
2022-10-18 10:45:21 +0800

    my god why need user input

--- src/sc2002_lab4/Shape2DApp.java
+++ src/sc2002_lab4/Shape2DApp.java
@@ -1,14 +1,49 @@
 package sc2002_lab4;
+import java.util.*;
 
 public class Shape2DApp {
     public static void main(String[] args) {
-        // Circle, triangle, and rectangle
-        Circle circle = new Circle(10);
-        Triangle triangle = new Triangle(25, 20);
-        Rectangle rectangle = new Rectangle(50, 20);
+        int userInput = 0;
+        Scanner sc = new Scanner(System.in);
 
-        circle.printArea();
-        triangle.printArea();
-        rectangle.printArea();
+        do {
+            printStartMessage();
+            userInput = sc.nextInt();
+            switch (userInput) {
+                case 1:
+                    System.out.println("Enter length and breadth: ");
+                    Rectangle rectangle = new Rectangle(sc.nextInt(), sc.nextInt());
+                    rectangle.printArea();
+                    break;
+                case 2:
+                    System.out.println("Enter length: ");
+                    Square square = new Square(sc.nextInt());
+                    square.printArea();
+                    break;
+                case 3:
+                    System.out.println("Enter base and height: ");
+                    Triangle triangle = new Triangle(sc.nextInt(), sc.nextInt());
+                    triangle.printArea();
+                    break;
+                case 4:
+                    System.out.println("Enter radius: ");
+                    Circle circle = new Circle(sc.nextInt());
+                    circle.printArea();
+                    break;
+                default:
+                    break;
+            }
+
+        } while (userInput != 0);
     }
+
+    public static void printStartMessage() {
+        System.out.println("Choose your shape");
+        System.out.println("0. Quit");
+        System.out.println("1. Rectangle");
+        System.out.println("2. Square");
+        System.out.println("3. Triangle");
+        System.out.println("4. Circle");
+    }
+
 }
--- src/sc2002_lab4/Shape3DApp.java
+++ src/sc2002_lab4/Shape3DApp.java
@@ -1,19 +1,55 @@
 package sc2002_lab4;
 
+import java.util.Scanner;
+
 public class Shape3DApp {
     public static void main(String[] args) {
-        Sphere sphere = new Sphere(10);
-        RegularTetrahedron regularTetrahedron = new RegularTetrahedron(10);
-        Cube cube = new Cube(10);
+        int userInput = 0;
+        Scanner sc = new Scanner(System.in);
 
-        Cone cone = new Cone(10, 10);
-        Cylinder cylinder = new Cylinder(10, 5);
+        do {
+            printStartMessage();
+            userInput = sc.nextInt();
+            switch (userInput) {
+                case 1:
+                    System.out.println("Enter radius and hypotenuse: ");
+                    Cone cone = new Cone(sc.nextInt(), sc.nextInt());
+                    cone.printArea();
+                    break;
+                case 2:
+                    System.out.println("Enter length, breadth and height: ");
+                    Cuboid cuboid = new Cuboid(sc.nextInt(), sc.nextInt(), sc.nextInt());
+                    cuboid.printArea();
+                    break;
+                case 3:
+                    System.out.println("Enter length: ");
+                    RegularTetrahedron regularTetrahedron = new RegularTetrahedron(sc.nextInt());
+                    regularTetrahedron.printArea();
+                    break;
+                case 4:
+                    System.out.println("Enter radius: ");
+                    Sphere sphere = new Sphere(sc.nextInt());
+                    sphere.printArea();
+                    break;
+                case 5:
+                    System.out.println("Enter radius and height: ");
+                    Cylinder cylinder = new Cylinder(sc.nextInt(), sc.nextInt());
+                    cylinder.printArea();
+                    break;
+                default:
+                    break;
+            }
 
-        sphere.printArea();
-        regularTetrahedron.printArea();
-        cube.printArea();
+        } while (userInput != 0);
+    }
 
-        cone.printArea();
-        cylinder.printArea();
+    public static void printStartMessage() {
+        System.out.println("Choose your shape");
+        System.out.println("0. Quit");
+        System.out.println("1. Cone");
+        System.out.println("2. Cuboid");
+        System.out.println("3. Regular Tetrahedron");
+        System.out.println("4. Sphere");
+        System.out.println("5. Cylinder");
     }
 }
